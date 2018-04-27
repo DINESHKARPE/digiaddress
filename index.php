f4d18dfff91510a7721849f4d8fdb80754abeb8d
Dinesh Karpe <contact@dineshkarpe.me>
2018-04-27 11:56:47 +0530

    Remove Unwanted Code

--- index.php
+++ index.php
@@ -3,17 +3,15 @@
 <head>
 
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-
 
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 
-
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXtUgIJI39fpYsM2y2FwAs0KynuS_qmP8">
-    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXtUgIJI39fpYsM2y2FwAs0KynuS_qmP8"/>
 
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+
     <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 
     <script src="js/functionality.js"/>
